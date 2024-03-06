207cf93d0376edbcbedb2df7077625dc1b08dade
Nima <sadeghinima090@gmail.com>
2024-03-06 16:56:34 +0330

    hero section complete

--- src/components/navbar/heroSection/hero.jsx
+++ src/components/navbar/heroSection/hero.jsx
@@ -1,8 +1,11 @@
 import './hero.css'
+import arrow from '../../../assets/dark-arrow.png'
 const Hero = () => {
     return (  <div className="hero container">
         <div className="hero-text">
-            
+            <h1>We Ensure Better education for a better word</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laboriosam ea repellendus quisquam voluptas nostrum molestias. Ut maiores aliquid sunt, illo nisi cumque nulla velit!</p>
+            <button className='btn'>Explore more <img src={arrow} alt="" /></button>
         </div>
     </div> );
 }
