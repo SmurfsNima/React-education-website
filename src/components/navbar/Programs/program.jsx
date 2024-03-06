5b1e8ca7aa133f2613d8ac51994d9230b1c8ec2c
Nima <sadeghinima090@gmail.com>
2024-03-06 19:01:32 +0330

    hover effect added

--- src/components/navbar/Programs/program.jsx
+++ src/components/navbar/Programs/program.jsx
@@ -1,11 +1,13 @@
-const Program = ({src,icon,text}) => {
-    return ( <div className="program">
-        <img src={src} alt="" />
-        <div className="caption">
-            <img src={icon} alt="" />
-            <p>{text}</p>
-        </div>
-    </div> );
-}
- 
+const Program = ({ src, icon, text }) => {
+  return (
+    <div className="program">
+      <img src={src} alt="" />
+      <div className="caption">
+        <img src={icon} alt="" />
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
 export default Program;
